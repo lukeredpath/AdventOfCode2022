03ac8daf08e8b9b82a25a7c45c5d18d45dd89bde
Luke Redpath <luke@lukeredpath.co.uk>
2022-12-06 11:52:55 +0000

    Day 6 part one and two

--- AOC2022/Sources/AOC2022/Solutions/Day_06.swift
+++ AOC2022/Sources/AOC2022/Solutions/Day_06.swift
@@ -4,10 +4,19 @@ import Parsing
 
 struct Day06: Solution {
     func runPartOne(input: Data) async throws -> String {
-        throw NotImplemented()
+        String(scanForDistinctCharacterGroup(in: utf8String(from: input), size: 4))
     }
 
     func runPartTwo(input: Data) async throws -> String {
-        throw NotImplemented()
+        String(scanForDistinctCharacterGroup(in: utf8String(from: input), size: 14))
+    }
+
+    private func scanForDistinctCharacterGroup(in string: String, size: Int) -> Int {
+        (size...string.count).first { offset in
+            let upperBound = string.index(string.startIndex, offsetBy: offset)
+            let lowerBound = string.index(upperBound, offsetBy: -size)
+            let chars = string[lowerBound..<upperBound]
+            return Set(chars).count == size
+        }!
     }
 }
--- AOC2022/Tests/AOC2022Tests/Day06Tests.swift
+++ AOC2022/Tests/AOC2022Tests/Day06Tests.swift
@@ -5,17 +5,43 @@ import XCTest
 
 final class Day06Tests: XCTestCase {
     let sampleInput = """
+    mjqjpqmgbljsphdztnvjfqwrcgsmlb
     """
 
-    func _testSampleSolution_PartOne() async throws {
+    func testSampleSolution_PartOne() async throws {
         let solution = Day06()
-        let answer = try await solution.runPartOne(input: sampleInput.data(using: .utf8)!)
-        XCTAssertEqual(answer, "")
+        var answer = try await solution.runPartOne(input: sampleInput.data(using: .utf8)!)
+        XCTAssertEqual(answer, "7")
+
+        answer = try await solution.runPartOne(input: "bvwbjplbgvbhsrlpgdmjqwftvncz".data(using: .utf8)!)
+        XCTAssertEqual(answer, "5")
+
+        answer = try await solution.runPartOne(input: "nppdvjthqldpwncqszvftbrmjlhg".data(using: .utf8)!)
+        XCTAssertEqual(answer, "6")
+
+        answer = try await solution.runPartOne(input: "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".data(using: .utf8)!)
+        XCTAssertEqual(answer, "10")
+
+        answer = try await solution.runPartOne(input: "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".data(using: .utf8)!)
+        XCTAssertEqual(answer, "11")
+
+        answer = try await solution.runPartOne(input: "aaaabcd".data(using: .utf8)!)
+        XCTAssertEqual(answer, "7")
     }
 
-    func _testSampleSolution_PartTwo() async throws {
+    func testSampleSolution_PartTwo() async throws {
         let solution = Day06()
-        let answer = try await solution.runPartTwo(input: sampleInput.data(using: .utf8)!)
-        XCTAssertEqual(answer, "")
+
+        var answer = try await solution.runPartTwo(input: "mjqjpqmgbljsphdztnvjfqwrcgsmlb".data(using: .utf8)!)
+        XCTAssertEqual(answer, "19")
+
+        answer = try await solution.runPartTwo(input: "bvwbjplbgvbhsrlpgdmjqwftvncz".data(using: .utf8)!)
+        XCTAssertEqual(answer, "23")
+
+        answer = try await solution.runPartTwo(input: "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".data(using: .utf8)!)
+        XCTAssertEqual(answer, "29")
+
+        answer = try await solution.runPartTwo(input: "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".data(using: .utf8)!)
+        XCTAssertEqual(answer, "26")
     }
 }
