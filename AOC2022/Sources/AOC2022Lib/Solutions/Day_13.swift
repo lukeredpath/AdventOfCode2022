05cce32535d6fec1671508843d462d357df524d9
Luke Redpath <luke@lukeredpath.co.uk>
2022-12-13 14:22:19 +0000

    Day 13 part one

--- AOC2022/Sources/AOC2022Lib/Solutions/Day_13.swift
+++ AOC2022/Sources/AOC2022Lib/Solutions/Day_13.swift
@@ -3,11 +3,111 @@ import Overture
 import Parsing
 
 struct Day13: Solution {
+    typealias Packet = [Value]
+    typealias PacketPair = (left: Packet, right: Packet)
+
+    indirect enum Value: Equatable {
+        case int(Int)
+        case list([Value])
+    }
+
+    enum ComparisonResult {
+        case correctOrder
+        case wrongOrder
+        case next
+    }
+
     func runPartOne(input: Data) async throws -> String {
-        throw NotImplemented()
+        try pipe(
+            utf8String,
+            Parsers.input.parse,
+            findIndicesForMatchingPackets,
+            { $0.reduce(0, +) },
+            String.init
+        )(input)
     }
 
     func runPartTwo(input: Data) async throws -> String {
         throw NotImplemented()
+    }
+
+    func findIndicesForMatchingPackets(in pairs: [PacketPair]) -> [Int] {
+        pairs.indices.reduce(into: []) { indices, index in
+            let pair = pairs[index]
+            if comparePackets(left: pair.left, right: pair.right) {
+                // Pair indices are 1-based so we should adjust.
+                indices.append(index + 1)
+            }
+        }
+    }
+
+    func comparePackets(left: Packet, right: Packet) -> Bool {
+        compareOrder(left: .list(left), right: .list(right)) == .correctOrder
+    }
+
+    func compareOrder(left: Value, right: Value) -> ComparisonResult {
+        switch (left, right) {
+        case let (.int(leftValue), .int(rightValue)):
+            return compareInts(left: leftValue, right: rightValue)
+        case let (.list(leftValue), .list(rightValue)):
+            return compareLists(left: leftValue, right: rightValue)
+        case let (.list(leftValue), .int):
+            return compareLists(left: leftValue, right: [right])
+        case let (.int, .list(rightValue)):
+            return compareLists(left: [left], right: rightValue)
+        }
+    }
+
+    private func compareInts(left: Int, right: Int) -> ComparisonResult {
+        if left == right { return .next }
+        return (left < right) ? .correctOrder : .wrongOrder
+    }
+
+    private func compareLists(left: [Value], right: [Value]) -> ComparisonResult {
+        let upperBound = max(left.indices.upperBound, right.indices.upperBound)
+        for index in 0..<upperBound {
+            guard left.indices.contains(index) else {
+                // We have run out of left items so the order is correct.
+                return .correctOrder
+            }
+            guard right.indices.contains(index) else {
+                // If we have run out of right items before left items the order is wrong.
+                return .wrongOrder
+            }
+            let result = compareOrder(left: left[index], right: right[index])
+            if result != .next { return result }
+        }
+        return .next
+    }
+
+    enum Parsers {
+        static var value: AnyParser<Substring, Value> {
+            OneOf {
+                Int.parser().map(Value.int)
+                Lazy { list.map(Value.list) }
+            }
+            .eraseToAnyParser()
+        }
+
+        static var list: AnyParser<Substring, [Value]> {
+            Parse {
+                "["
+                Many { value } separator: { "," }
+                "]"
+            }
+            .eraseToAnyParser()
+        }
+
+        static let packetPair = Parse {
+            list
+            Whitespace(1, .vertical)
+            list
+        }.map { (left: $0, right: $1) }
+
+        static let input = Many {
+            packetPair
+        } separator: {
+            Whitespace(2, .vertical)
+        }
     }
 }
--- AOC2022/Tests/AOC2022Tests/Day13Tests.swift
+++ AOC2022/Tests/AOC2022Tests/Day13Tests.swift
@@ -5,16 +5,143 @@ import XCTest
 
 final class Day13Tests: XCTestCase {
     let sampleInput = """
+    [1,1,3,1,1]
+    [1,1,5,1,1]
+
+    [[1],[2,3,4]]
+    [[1],4]
+
+    [9]
+    [[8,7,6]]
+
+    [[4,4],4,4]
+    [[4,4],4,4,4]
+
+    [7,7,7,7]
+    [7,7,7]
+
+    []
+    [3]
+
+    [[[]]]
+    [[]]
+
+    [1,[2,[3,[4,[5,6,7]]]],8,9]
+    [1,[2,[3,[4,[5,6,0]]]],8,9]
+
     """
 
-    func _testSampleSolution_PartOne() async throws {
-        let solution = Day13()
+    let solution = Day13()
+
+    func testCompareInts() {
+        XCTAssertEqual(
+            solution.compareOrder(left: .int(1), right: .int(2)),
+            .correctOrder
+        )
+        XCTAssertEqual(
+            solution.compareOrder(left: .int(2), right: .int(1)),
+            .wrongOrder
+        )
+        XCTAssertEqual(
+            solution.compareOrder(left: .int(2), right: .int(2)),
+            .next
+        )
+    }
+
+    func testCompareLists() {
+        XCTAssertEqual(
+            solution.compareOrder(
+                left: .list([.int(1), .int(1), .int(3), .int(1), .int(1)]),
+                right: .list([.int(1), .int(1), .int(5), .int(1), .int(1)])
+            ),
+            .correctOrder
+        )
+        XCTAssertEqual(
+            solution.compareOrder(
+                left: .list([.int(1), .int(1), .int(5), .int(1), .int(1)]),
+                right: .list([.int(1), .int(1), .int(3), .int(1), .int(1)])
+            ),
+            .wrongOrder
+        )
+        XCTAssertEqual(
+            solution.compareOrder(
+                left: .list([.int(1), .int(1), .int(1), .int(1), .int(1)]),
+                right: .list([.int(1), .int(1), .int(1), .int(1), .int(1)])
+            ),
+            .next
+        )
+        XCTAssertEqual(
+            solution.compareOrder(
+                left: .list([.int(1), .int(1), .int(1)]),
+                right: .list([.int(1), .int(1), .int(1), .int(1), .int(1)])
+            ),
+            .correctOrder
+        )
+        XCTAssertEqual(
+            solution.compareOrder(
+                left: .list([.int(1), .int(1), .int(1), .int(1), .int(1)]),
+                right: .list([.int(1), .int(1), .int(1)])
+            ),
+            .wrongOrder
+        )
+    }
+
+    func testCompareNestedLists() {
+        XCTAssertEqual(
+            solution.compareOrder(
+                left: .list([.int(9)]),
+                right: .list([.list([.int(8), .int(7), .int(6)])])
+            ),
+            .wrongOrder
+        )
+        XCTAssertEqual(
+            solution.compareOrder(
+                left: .list([.list([.int(4), .int(4)]), .int(4), .int(4)]),
+                right: .list([.list([.int(4), .int(4)]), .int(4), .int(4), .int(4)])
+            ),
+            .correctOrder
+        )
+        XCTAssertEqual(
+            solution.compareOrder(
+                left: .list([.list([.list([])])]),
+                right: .list([.list([])])
+            ),
+            .wrongOrder
+        )
+    }
+
+    func testParseList() throws {
+        XCTAssertNoDifference(
+            try Day13.Parsers.list.parse("[1,2,3]"),
+            [.int(1), .int(2), .int(3)]
+        )
+        XCTAssertNoDifference(
+            try Day13.Parsers.list.parse("[[1],2,3]"),
+            [.list([.int(1)]), .int(2), .int(3)]
+        )
+        XCTAssertNoDifference(
+            try Day13.Parsers.list.parse("[[1],2,[[]]]"),
+            [.list([.int(1)]), .int(2), .list([.list([])])]
+        )
+    }
+
+    func testParsePair() throws {
+        let input = """
+        [1,1,3,1,1]
+        [1,1,5,1,1]
+        """
+
+        let pair = try Day13.Parsers.packetPair.parse(input)
+        XCTAssertNoDifference(pair.left, [.int(1), .int(1), .int(3), .int(1), .int(1)])
+        XCTAssertNoDifference(pair.right, [.int(1), .int(1), .int(5), .int(1), .int(1)])
+    }
+
+    func testSampleSolution_PartOne() async throws {
         let answer = try await solution.runPartOne(input: sampleInput.data(using: .utf8)!)
-        XCTAssertEqual(answer, "")
+        XCTAssertEqual(answer, "13")
     }
 
     func _testSampleSolution_PartTwo() async throws {
-        let solution = Day13()
         let answer = try await solution.runPartTwo(input: sampleInput.data(using: .utf8)!)
         XCTAssertEqual(answer, "")
     }
